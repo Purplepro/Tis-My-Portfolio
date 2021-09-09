d334ac8a113c24a6d6f9d907270a7f6605083512
Sebastian G <gayle054@gmail.com>
2021-09-09 08:00:30 -0700

    working on navbar

--- divine-portfolio/src/components/Navbar/NavLinks.js
+++ divine-portfolio/src/components/Navbar/NavLinks.js
@@ -7,6 +7,10 @@ const NavLinks = (props) => {
     return ( 
         <ul className="link-bunch">
 
+            <li>
+                <img src={sevslogo} className="SevsLogo"  alt="sebastians logo of his initials"/>
+            </li>
+
         <li onClick={() => props.isMobile && props.closeMobileMenu()}>
            <NavLink className="nav-link" to='/'>Home</NavLink> 
             </li>
@@ -14,13 +18,9 @@ const NavLinks = (props) => {
         <li onClick={() => props.isMobile && props.closeMobileMenu()}>
         <NavLink className="nav-link" to='/resumepage'>Resume</NavLink>
         </li>
-
-            <li>
-                <img src={sevslogo} className="SevsLogo"  alt="sebastians logo of his initials"/>
-            </li>
         
         <li onClick={() => props.isMobile && props.closeMobileMenu()}>
-            <NavLink className="nav-link" id="contact-link" to='/contact'>Contact</NavLink>
+            <NavLink id="contact-link" to='/contact'>Contact</NavLink>
         </li>
 
     
