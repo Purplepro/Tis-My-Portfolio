a35c8888dd0ca3690c3100d3f6f079af853cc85a
Sebastian G <gayle054@gmail.com>
2021-09-20 05:31:34 -0700

    added twitter link

--- divine-portfolio/src/components/Footer/Footer.js
+++ divine-portfolio/src/components/Footer/Footer.js
@@ -12,7 +12,7 @@ const Footer = () => {
           
                  <div className="footer-child"><a href="https://github.com/Purplepro"><ImGithub color="white" className="social-icons" /></a></div>
                  <div className="footer-child"><a href="https://www.linkedin.com/in/sebastian-gayle-500897167/"><ImLinkedin color="white" className="social-icons" /></a></div>
-                <div className="footer-child"><a href=""><ImTwitter color="white" className="social-icons"/></a></div>
+                <div className="footer-child"><a href="https://twitter.com/Seviocreatives"><ImTwitter color="white" className="social-icons"/></a></div>
            
         </div>
     )
